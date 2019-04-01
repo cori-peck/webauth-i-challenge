d0641680f667376d171b1b5cb48d795c93bb0f3b
cori-peck <cori.peck@ymail.com>
2019-04-01 17:50:56 -0500

    register is functional with hashed password, login is not

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const express = require('express');
 const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile')
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const db = require('./users/users');
 
 const server = express();
-const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.status(200).send("Intro to Authentication - Challenge I")
+})
+
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+        db
+        .add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Could not complete registration"})
+        })
+    }
+)
+
+
+server.post('/api/login', (req, res) => {
+    let user = req.body;
+    if (username && username.password) {
+        db
+        .findBy({ username: user.username})
+        .first()
+        .then(info => {
+            if (info && bcrypt.compareSync(user.password, info.password)) {
+                res.status(200).json({ message: `Welcome ${info.username}`})
+            } else {
+                res.status(401).json({ message: "Please enter valid credentials"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    } else {
+        res.status(400).json({ message: "Enter a username and password" })
+    }
+})
 
 const port = process.env.PORT || 5050;
 server.listen(port, () => 
