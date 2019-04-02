53c4597c1561253548ce8c7df6df25020a88d96d
cori-peck <cori.peck@ymail.com>
2019-04-02 17:34:53 -0500

    correction, get users is now restr and throws correct err

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require('./data/dbConfig');
 const Users = require('./users/users');
 const session = require('express-session');
 const sessionConfig = require('./auth/session-config.js');
+const restricted = require('./auth/restricted-middleware.js');
 
 const server = express();
 
@@ -79,24 +80,6 @@ server.get('/api/users', restricted, (req, res) => {
     })
     .catch(err => res.send(err))
 })
-
-function restricted(req, res, next) {
-    const { username, password } = req.headers;
-
-    Users
-        .findBy({ username })
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(password, user.password)) {
-            next();
-        } else {
-            res.status(401).json({ message: "You do not have permission to access" })
-        }
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
-}
 
 
 
